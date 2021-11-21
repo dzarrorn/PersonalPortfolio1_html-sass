d9d5e0d9638945f5df5d732547b6fd0bf9a23468
dzarrorn <zarrorn@gmail.com>
2021-11-21 17:53:49 +0700

    wow sekali

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#burger').onclick = () => {
     navbar.classList.toggle('active');
 }
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
