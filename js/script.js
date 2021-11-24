39dc339bb3390e3da086407ad9b3059b3124dcb7
dzarrorn <zarrorn@gmail.com>
2021-11-24 07:17:08 +0700

    done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 let navbar = document.querySelector('.navbar');
+let scrollBar = document.querySelector('.scroll-bar');
 document.querySelector('#burger').onclick = () => {
     navbar.classList.toggle('active');
 }
 window.onscroll = () => {
     navbar.classList.remove('active');
+    scrollBarIndicator()
+}
+
+function scrollBarIndicator() {
+    let maxHeight = window.document.body.scrollHeight - window.innerHeight;
+    let percentage = ((window.scrollY) / maxHeight) * 100;
+    scrollBar.style.width = percentage + '%';
 }
